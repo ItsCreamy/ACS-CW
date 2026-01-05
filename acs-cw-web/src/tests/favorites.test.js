301c55d52b02794bdb0e27698ecded9857026a5c
Chanul Udugampola <chanuludugampola@gmail.com>
2026-01-05 10:55:21 +0530

    Remove unused import from favorites test
    The 'screen' import from '@testing-library/react' was removed in favorites.test.js as it was not being used.

--- acs-cw-web/src/tests/favorites.test.js
+++ acs-cw-web/src/tests/favorites.test.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import { render, screen, act } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import { FavoritesProvider, useFavorites } from '../context/FavoritesContext';
 
 const mockProperty = {
